8eded2cc99467e241e64a908a51118bfe2651ee6
Ankit Kumar <ankit.kumar@oge.co>
2025-03-04 10:44:08 +0530

    changes

--- src/app/pages/Home/Home.jsx
+++ src/app/pages/Home/Home.jsx
@@ -7,7 +7,16 @@ function Home() {
     return (
         <>
             <Header />
-            <div className="container m-0 home-img"></div>
+            <div className="container d-flex justify-content-center align-items-center m-0 home-img">
+                <div className="card-body">
+                    <h1>Search</h1>
+                    <p>Search high-resolution images from Unsplash</p>
+                    <div className="search d-flex justify-content-between">
+                        <input type="text" placeholder="Enter your keywords..." />  
+                        <img className="d-flex align-items-center" style={{ cursor: "pointer" }} src="src/assets/img/Search.svg" />
+                    </div>
+                </div>
+            </div>
         </>
     );
 }
